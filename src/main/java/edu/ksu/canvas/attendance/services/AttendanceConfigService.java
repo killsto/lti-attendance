d181f2ea218fed047efb8efd8465b7facd55c6d4
Dustin Hammett <dhammett@ksu.edu>
2018-06-01 17:05:41 -0500

    Deployable to Tomcat using H2 embedded DB.
    ID 1054

--- src/main/java/edu/ksu/canvas/attendance/services/AttendanceConfigService.java
+++ src/main/java/edu/ksu/canvas/attendance/services/AttendanceConfigService.java
@@ -1,8 +1,8 @@
 package edu.ksu.canvas.attendance.services;
 
+import edu.ksu.canvas.attendance.entity.ConfigItem;
 import edu.ksu.canvas.attendance.repository.ConfigRepository;
 import edu.ksu.lti.launch.service.ConfigService;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,10 +15,13 @@ public class AttendanceConfigService implements ConfigService {
     @Override
     public String getConfigValue(String property) {
         //Because these are stored as "COMMON" attributes, we need to pull them slightly differently
-        if (property.equals("canvas_url") || property.equals("canvas_url_2")){
+        if ("canvas_url".equals(property) || "canvas_url_2".equals(property)){
             return configRepository.findByLtiApplicationAndKey("COMMON", property).getValue();
+        } else if ("oauth_client_id".equals(property) || "oauth_client_secret".equals(property)) {
+            return configRepository.findByLtiApplicationAndKey("Attendance", property).getValue();
         }
-        return configRepository.findByLtiApplicationAndKey("Attendance", property).getValue();
+        final ConfigItem configItem = configRepository.findByLtiApplicationAndKey("Attendance", property);
+        return configItem.getValue();
     }
 
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,9 @@
-insert into LTI_KEY (key_id, key_key, secret, consumer_profile) values (1, '0c5ae2d2beb3197b9ed17cc4863b3a9b', '418c40d4f2359e4a4d7c061beea88921', null);
+--Note: This file MUST BE 1 line per statement, if your statement is long, it CANNOT (hard) wrap to a new line
+insert into LTI_KEY (key_id, key_key, secret, consumer_profile) values (1, '5396b5be4a75b40e8131', '9564a64cea7ab33a45ac', null);
 
-insert into CONFIG_ITEM (config_id, lti_application, config_key, config_value) values (1, 'COMMON', 'canvas_url', 'k-state.test.instructure.com');
-
-insert into CONFIG_ITEM (config_id, lti_application, config_key, config_value) values (2, 'COMMON', 'canvas_url_2', 'k-state.test.instructure.com');
+insert into CONFIG_ITEM (config_id, lti_application, config_key, config_value) values (1, 'COMMON', 'canvas_url', 'https://k-state.test.instructure.com');
+insert into CONFIG_ITEM (config_id, lti_application, config_key, config_value) values (2, 'COMMON', 'canvas_url_2', 'https://k-state.test.instructure.com');
 
 insert into CONFIG_ITEM (config_id, lti_application, config_key, config_value) values (3, 'Attendance', 'launch_url', 'https://localhost:8443/launch');
+insert into CONFIG_ITEM (config_id, lti_application, config_key, config_value) values (4, 'Attendance', 'oauth_client_id', '17260000000000055');
+insert into CONFIG_ITEM (config_id, lti_application, config_key, config_value) values (5, 'Attendance', 'oauth_client_secret','HV3cbjepezScPcuM4OGKuV4P3JXdAtHKCYGIGuLSLWXal9LxEdWD4endR6XXrNvf');
