23b3ba0f22bc78d5c9f0d0bc964a15318589d81b
Dustin Hammett <dhammett@ksu.edu>
2018-06-01 17:07:41 -0500

    Code cleanup
    Removing unnecessary conditionals I was only using to facilitate breakpoints when debugging.

--- src/main/java/edu/ksu/canvas/attendance/services/AttendanceConfigService.java
+++ src/main/java/edu/ksu/canvas/attendance/services/AttendanceConfigService.java
@@ -1,6 +1,5 @@
 package edu.ksu.canvas.attendance.services;
 
-import edu.ksu.canvas.attendance.entity.ConfigItem;
 import edu.ksu.canvas.attendance.repository.ConfigRepository;
 import edu.ksu.lti.launch.service.ConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +16,8 @@ public class AttendanceConfigService implements ConfigService {
         //Because these are stored as "COMMON" attributes, we need to pull them slightly differently
         if ("canvas_url".equals(property) || "canvas_url_2".equals(property)){
             return configRepository.findByLtiApplicationAndKey("COMMON", property).getValue();
-        } else if ("oauth_client_id".equals(property) || "oauth_client_secret".equals(property)) {
-            return configRepository.findByLtiApplicationAndKey("Attendance", property).getValue();
         }
-        final ConfigItem configItem = configRepository.findByLtiApplicationAndKey("Attendance", property);
-        return configItem.getValue();
+        return configRepository.findByLtiApplicationAndKey("Attendance", property).getValue();
     }
 
 }
